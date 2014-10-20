241b0fc3f9cccb193c0a367f991f194b6a0f83de
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-10-20 22:16:38 +0100

    basic push messaging

--- CatchUp/AppDelegate.swift
+++ CatchUp/AppDelegate.swift
@@ -9,9 +9,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         Parse.setApplicationId("m2lzGhNQcPev2ivKqfRRVr4ZP12k0LDP79FzMkTl", clientKey:"E9GdH2RRXh1k88SnjtRiEl6tCbfcTshi5MFsVAgw")
         
-//        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "user_id")
-
+        let settings = UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound | UIUserNotificationType.Badge, categories: nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+        UIApplication.sharedApplication().registerForRemoteNotifications()
+        
         return true
+    }
+    
+    func application(application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: NSData) {
+        var currentInstallation = PFInstallation.currentInstallation()
+        currentInstallation.setDeviceTokenFromData(deviceToken)
+        currentInstallation.saveEventually()
+    }
+    
+    func application(application: UIApplication, didReceiveRemoteNotification userInfo: [NSObject : AnyObject]) {
+        PFPush.handlePush(userInfo)
     }
 
     func applicationWillResignActive(application: UIApplication) {
